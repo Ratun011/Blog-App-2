ac9db970e9139304e5c6760fc1d382dd503d5abe
Ratun <www.ratunrahman@gmail.com>
2020-12-12 18:32:42 +0600

    first push

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,95 @@
+import React,{useState,useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet ,View,FlatList ,Button} from 'react-native';
+import { Header} from 'react-native-elements';
+import {AuthContext} from './../providers/AuthProvider';
+import { Ionicons } from '@expo/vector-icons';
+import PostWrite from './../components/PostWrite';
+import ShowPost from './../components/PostShow';
+
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+//import { getDataJSON, getAllKeys} from '../functions/AsyncStorageFunctions';
+
+
+const  HomeScreen =({navigation})=> {
+    const [posts,setPosts]=useState([]);
+    const [reload,setReload]=useState(false)
+    const getPosts = async ()=>{
+        setLoading(true);
+        await firebase.firestore().collection('posts').orderBy("created_at", "desc").onSnapshot((querySnapshot)=>{
+            let temp_posts = []
+            querySnapshot.forEach((doc)=>{
+                temp_posts.push({
+                    id: doc.id,
+                    data: doc.data(),
+                });
+            });
+            setPosts(temp_posts);
+            setLoading(false);
+        }).then().catch((error)=>{
+            setLoading(false);
+            alert(error);
+        });
+        }
+    }
+
+    useEffect(()=>{
+        getPosts();
+    },[]);
+    
+
+    return(
+        <AuthContext.Consumer>
+            {(auth)=>
+            (
+                <View style={styles.container}>
+                    <StatusBar style="light"/>
+                    <Header
+                    containerStyle={{
+                    backgroundColor: '#404040',
+                    justifyContent: 'space-around',}}
+                    leftComponent={<Ionicons name="md-menu" size={25} color="white" onPress={()=>{
+                        navigation.openDrawer();
+                    }}/>}
+                    centerComponent={{ text: 'Home', style: { fontSize:20,color: '#fff' } }}
+                    rightComponent={<Ionicons name="md-lock" size={25} color="white" 
+                    onPress={()=>{
+                        auth.setisLogged(false);
+                        auth.setcurrentUser({});
+                    }}/>}
+                    />
+                    
+                        <PostWrite user={auth.currentUser} />
+                        <FlatList
+                        data={posts}
+                        onRefresh={getPosts}
+                        refreshing={reload}
+                        renderItem={function ({ item }) {
+                            return (
+                            <ShowPost
+                                content={item}
+                                currentuser={auth.currentUser}
+                                navigation={navigation}
+                            />
+                            );
+                        }}
+                        keyExtractor={(item, index) => index.toString()}
+                        />
+                </View>
+                
+            )}
+        </AuthContext.Consumer>
+    );
+
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: "center",
+        
+    },
+});
+
+export default HomeScreen;
